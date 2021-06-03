4b36967d2bce1d33aaffaaf9c58b1d6f10e8ad1c
Frank Th. van de Ven <vandevenator@gmail.com>
2021-06-03 16:19:16 +0800

    Release 4.0.135

--- VenturaSQL.NETStandard/Properties/AssemblyInfo.cs
+++ VenturaSQL.NETStandard/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 using VenturaSQL;
 
-[assembly: ReleaseDate(2021, 5, 26, 7, 19 , 44)]
+[assembly: ReleaseDate(2021, 6, 1, 3, 8 , 51)]
 
 [assembly: AssemblyTitle("VenturaSQL runtime for .Net Standard 2.0. Works with .NET Core (Windows/macOS/Linux) and .NET Framework.")]
 [assembly: AssemblyDescription("VenturaSQL runtime for .Net Standard 2.0. Works with .NET Core (Windows/macOS/Linux) and .NET Framework.")]
 
-[assembly: AssemblyVersion("4.0.135.0")]
-[assembly: AssemblyFileVersion("4.0.135.0")]
-[assembly: AssemblyInformationalVersion("4.0.135")]
+[assembly: AssemblyVersion("4.0.141.0")]
+[assembly: AssemblyFileVersion("4.0.141.0")]
+[assembly: AssemblyInformationalVersion("4.0.141")]
 
 [assembly: AssemblyCompany("Frank Th. van de Ven")]
 [assembly: AssemblyProduct("VenturaSQL™")]
--- VenturaSQLStudio/Properties/AssemblyInfo.cs
+++ VenturaSQLStudio/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using VenturaSQL;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: ReleaseDate(2021, 5, 26, 7, 19 , 44)]
+[assembly: ReleaseDate(2021, 6, 1, 3, 8 , 51)]
 
 // Version information for an assembly consists of the following four values: Major Version, Minor Version, Build Number, Revision
 // You can specify all the values or you can default the Build and Revision Numbers 
@@ -37,9 +37,9 @@ using VenturaSQL;
 //
 // VenturaSQL uses version as: Major.Minor.Patch The Revision part always remains fixed at zero.
 //
-[assembly: AssemblyVersion("4.0.135.0")]
-[assembly: AssemblyFileVersion("4.0.135.0")]
-[assembly: AssemblyInformationalVersion("4.0.135")] // <-- This is the 'general' product version, for example: "1.0.1 or 1.0.1-rc or 1.1.2-beta4 or 2.0.0-alpha3"
+[assembly: AssemblyVersion("4.0.141.0")]
+[assembly: AssemblyFileVersion("4.0.141.0")]
+[assembly: AssemblyInformationalVersion("4.0.141")] // <-- This is the 'general' product version, for example: "1.0.1 or 1.0.1-rc or 1.1.2-beta4 or 2.0.0-alpha3"
 
 // General Information about an assembly is controlled through the following set of attributes.
 [assembly: AssemblyCompany("Frank Th. van de Ven")]
