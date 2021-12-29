1e95a20884db528ab0ddfa10d38d34dc1911bda0
Frank Th. van de Ven <vandevenator@gmail.com>
2021-12-29 07:31:45 +0100

    Upgraded to .NET 6

--- VenturaSQL.AspNetCore.Server/Properties/AssemblyInfo.cs
+++ VenturaSQL.AspNetCore.Server/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 using VenturaSQL;
 
-[assembly: ReleaseDate(2021, 6, 13, 6, 58 , 21)]
+[assembly: ReleaseDate(2021, 11, 9, 14, 11 , 29)]
 
 [assembly: AssemblyTitle("VenturaSQL runtime server code for ASP.NET Core on .NET 5.0 or higher. Works with Windows, macOS and Linux.")]
 [assembly: AssemblyDescription("VenturaSQL runtime server code for ASP.NET Core on .NET 5.0 or higher. Works with Windows, macOS and Linux.")]
 
-[assembly: AssemblyVersion("4.0.145.0")]
-[assembly: AssemblyFileVersion("4.0.145.0")]
-[assembly: AssemblyInformationalVersion("4.0.145")]
+[assembly: AssemblyVersion("4.1.147.0")]
+[assembly: AssemblyFileVersion("4.1.147.0")]
+[assembly: AssemblyInformationalVersion("4.1.147")]
 
 [assembly: AssemblyCompany("Frank Th. van de Ven")]
 [assembly: AssemblyProduct("VenturaSQL™")]
--- VenturaSQL.NETStandard/Properties/AssemblyInfo.cs
+++ VenturaSQL.NETStandard/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 using VenturaSQL;
 
-[assembly: ReleaseDate(2021, 6, 13, 6, 58 , 21)]
+[assembly: ReleaseDate(2021, 11, 9, 14, 11 , 29)]
 
 [assembly: AssemblyTitle("VenturaSQL runtime for .Net Standard 2.0. Works with .NET Core (Windows/macOS/Linux) and .NET Framework.")]
 [assembly: AssemblyDescription("VenturaSQL runtime for .Net Standard 2.0. Works with .NET Core (Windows/macOS/Linux) and .NET Framework.")]
 
-[assembly: AssemblyVersion("4.0.145.0")]
-[assembly: AssemblyFileVersion("4.0.145.0")]
-[assembly: AssemblyInformationalVersion("4.0.145")]
+[assembly: AssemblyVersion("4.1.147.0")]
+[assembly: AssemblyFileVersion("4.1.147.0")]
+[assembly: AssemblyInformationalVersion("4.1.147")]
 
 [assembly: AssemblyCompany("Frank Th. van de Ven")]
 [assembly: AssemblyProduct("VenturaSQL™")]
